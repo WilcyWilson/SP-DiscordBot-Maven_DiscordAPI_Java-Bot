36adebf938aa3df406615c0ed49f7163a48ef4d1
Wilson Shrestha <wilsonshresthaapps@gmail.com>
2021-10-07 18:39:59 +0545

    added picture

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - Maven is a build automation tool that handles dependencies for different types of projects.
 - Create a Discord Bot through your discord account first and add it to your discord server.
 - Copy the properties and dependency to the pom.xml from Java Discord API Github.
+
+## Working Bot
+
+![Demo](demo.png)
--- src/main/java/com/project/discordbot/Main.java
+++ src/main/java/com/project/discordbot/Main.java
@@ -9,9 +9,10 @@ import net.dv8tion.jda.api.entities.Activity;
 public class Main {
 
 	public static void main(String[] args) {
-		JDABuilder jdaBuilder = JDABuilder.createDefault("ODk1NjM5NDc5NTg1NDc2Njg4.YV7fRA.2vK0TCH-SukvCm2zXnJ7C_smtVE");
+		JDABuilder jdaBuilder = JDABuilder.createDefault("fake-token");
 		jdaBuilder.setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jdaBuilder.setActivity(Activity.playing("Game"));
+		
 		try {
 			jdaBuilder.build();
 		} catch (LoginException e) {
