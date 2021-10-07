9528596da80fbd76872e6b52d460a7e15dc0978f
Wilson Shrestha <wilsonshresthaapps@gmail.com>
2021-10-07 20:42:12 +0545

    fakebot

--- src/main/java/com/project/discordbot/Main.java
+++ src/main/java/com/project/discordbot/Main.java
@@ -10,7 +10,7 @@ import net.dv8tion.jda.api.entities.Activity;
 public class Main {
 
 	public static void main(String[] args) {
-		JDABuilder jdaBuilder = JDABuilder.createDefault("ODk1Njg1MDkxMjc4MzUyNDI0.YV8Jvg.VZAhCEJj0LaFLQz9FXDkyNDvhWI");
+		JDABuilder jdaBuilder = JDABuilder.createDefault("fake token");
 		jdaBuilder.setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jdaBuilder.setActivity(Activity.playing("Game"));
 		
