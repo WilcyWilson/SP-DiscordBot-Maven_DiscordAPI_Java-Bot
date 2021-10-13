6e3141c77d433dc0d1a49b2016e06ac366332c91
Wilson Shrestha <wilsonshresthaapps@gmail.com>
2021-10-13 08:11:57 +0545

    added GuildMemberJoinEvent

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 - Message Embed provides a more descriptive to show messages in discord.
 - It consists of option like colors,description,footer,title,images,thumbnail etc.
 
-### Discord bot that lists the current users in the server and also shows number of users in the server at the bottom
+### Discord bot that welcomes the user that joins the channel and also shows date with ?date command
+
+![Welcome](new.png)
 
 #### Message Embed
 ![Embed](embed.png)
+
+### Note: Removed list of users. Bugged.
 
 
--- src/main/java/com/project/discordbot/Main.java
+++ src/main/java/com/project/discordbot/Main.java
@@ -6,12 +6,13 @@ import events.MessageListener;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
-
 	public static void main(String[] args) {
-		String token = "fake-token";
+		String token = "ODk1Njg1MDkxMjc4MzUyNDI0.YV8Jvg.drVelGdEG2CQvnOOy9WbNnn8QLE";
 		JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+		jdaBuilder.enableIntents(GatewayIntent.GUILD_MEMBERS); // Enables Presence tracking and tracks member activity
 		jdaBuilder.setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jdaBuilder.setActivity(Activity.playing("Potato"));
 		jdaBuilder.addEventListeners(new MessageListener());
